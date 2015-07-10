33a8b745ca4da9da64c1d651c29c7c1b6f7eeb00
Leucemidus <dr.sodor@gmail.com>
2015-07-09 19:56:18 -0800

    Visual Studio 2012 C# AccessB LED demo
    This is a Visual Studio 2012 project on C#, it work with an PIC18F2550
    programmed with AccessB firmware.
    
    This show the use of GPIO to turn ON/OFF a simple LED connected to RA0
    pin.

---
+++ Simple LED Demo/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Simple_LED_Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFindDev = new System.Windows.Forms.Button();
+            this.btnConfig = new System.Windows.Forms.Button();
+            this.btnLEDOn = new System.Windows.Forms.Button();
+            this.btnLEDOff = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnFindDev
+            // 
+            this.btnFindDev.Location = new System.Drawing.Point(12, 27);
+            this.btnFindDev.Name = "btnFindDev";
+            this.btnFindDev.Size = new System.Drawing.Size(198, 32);
+            this.btnFindDev.TabIndex = 0;
+            this.btnFindDev.Text = "Find Device";
+            this.btnFindDev.UseVisualStyleBackColor = true;
+            this.btnFindDev.Click += new System.EventHandler(this.btnFindDev_Click);
+            // 
+            // btnConfig
+            // 
+            this.btnConfig.Enabled = false;
+            this.btnConfig.Location = new System.Drawing.Point(12, 65);
+            this.btnConfig.Name = "btnConfig";
+            this.btnConfig.Size = new System.Drawing.Size(198, 32);
+            this.btnConfig.TabIndex = 0;
+            this.btnConfig.Text = "Configure RA0 as output";
+            this.btnConfig.UseVisualStyleBackColor = true;
+            this.btnConfig.Click += new System.EventHandler(this.btnConfig_Click);
+            // 
+            // btnLEDOn
+            // 
+            this.btnLEDOn.Enabled = false;
+            this.btnLEDOn.Location = new System.Drawing.Point(12, 103);
+            this.btnLEDOn.Name = "btnLEDOn";
+            this.btnLEDOn.Size = new System.Drawing.Size(198, 32);
+            this.btnLEDOn.TabIndex = 0;
+            this.btnLEDOn.Text = "LED On";
+            this.btnLEDOn.UseVisualStyleBackColor = true;
+            this.btnLEDOn.Click += new System.EventHandler(this.btnLEDOn_Click);
+            // 
+            // btnLEDOff
+            // 
+            this.btnLEDOff.Enabled = false;
+            this.btnLEDOff.Location = new System.Drawing.Point(12, 141);
+            this.btnLEDOff.Name = "btnLEDOff";
+            this.btnLEDOff.Size = new System.Drawing.Size(198, 32);
+            this.btnLEDOff.TabIndex = 0;
+            this.btnLEDOff.Text = "LED Off";
+            this.btnLEDOff.UseVisualStyleBackColor = true;
+            this.btnLEDOff.Click += new System.EventHandler(this.btnLEDOff_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(58, 11);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(109, 13);
+            this.lblStatus.TabIndex = 1;
+            this.lblStatus.Text = "Status: Disconnected";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(222, 184);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnLEDOff);
+            this.Controls.Add(this.btnLEDOn);
+            this.Controls.Add(this.btnConfig);
+            this.Controls.Add(this.btnFindDev);
+            this.Name = "Form1";
+            this.Text = "Simple LED Demo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFindDev;
+        private System.Windows.Forms.Button btnConfig;
+        private System.Windows.Forms.Button btnLEDOn;
+        private System.Windows.Forms.Button btnLEDOff;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ Simple LED Demo/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simple_LED_Demo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        AccessB AccB = new AccessB(); //Declare the AccessB object first
+
+        private void btnFindDev_Click(object sender, EventArgs e)
+        {
+            //VID and PID used by the USB module in the PIC, these values are declarated in the AccessB_winusbapi file
+            //so isn't needed to assign that values but this is show here if you change the PID and VID from the PIC 
+            //firmware you know how to change the default values.
+
+            AccB.PID = 0x003F;//You can comment these lines and the method will find the device if is connected
+            AccB.VID = 0x04d8;
+
+            if (AccB.FindDevHID() == true)
+            {
+                lblStatus.Text = "Status: Connected";
+                btnConfig.Enabled = true;
+                btnLEDOff.Enabled = true;
+                btnLEDOn.Enabled = true;
+            }
+            else
+            {
+                lblStatus.Text = "Status: Disconnected";
+            }
+        }
+
+        private void btnConfig_Click(object sender, EventArgs e)
+        {
+            AccB.TRISAbits_TRISA0 = 0; //Set pin RA0 of the PIC as output
+            //AccB.TRISA = 0; //All the PORTA as output
+        }
+
+        private void btnLEDOn_Click(object sender, EventArgs e)
+        {
+            AccB.LATAbits_LATA0 = 1; //Set pin RA0 high, LED On
+            //AccB.LATA = 0xFF; //Complete PORTA pins set to ones
+            //AccB.LATA = 0xAA;
+            //AccB.LATAbits_LATA3 = 1;
+        }
+
+        private void btnLEDOff_Click(object sender, EventArgs e)
+        {
+            AccB.LATAbits_LATA0 = 0; //Set pin RA0 low, LED Off
+            //AccB.LATA = 0; //Complete PORTA pins set to zeroes
+        }
+    }
+}
